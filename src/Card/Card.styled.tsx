c530cc6aa7ccce10840bfc6fc56814427e2db0b7
Daniel Gary <daniel@gatewayapps.com>
2019-10-11 08:42:52 -0500

    added Card component

---
+++ src/Card/Card.styled.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #fafafa;
+  border: 2px solid #a1a1a1;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  padding: 10px;
+  position: relative;
+`
+
+export const RemoveButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
+export const Body = styled.div``
+
+export const Header = styled.div`
+  margin-bottom: 5px;
+`
+
+export const Title = styled.div`
+  font-weight: 600;
+  font-size: 1.1em;
+`
+
+export const Subtitle = styled.div`
+  color: #7a7a7a;
+  font-size: 0.8em;
+`
---
+++ src/Card/index.tsx
@@ -0,0 +1,39 @@
+import React, { CSSProperties, ReactNode, FC } from 'react'
+import * as Styled from './Card.styled'
+
+export interface ICardProps {
+  style: CSSProperties
+  title: ReactNode
+  subtitle: ReactNode
+  removeButton: boolean
+  renderHeader?: () => ReactNode
+  onRemove?: () => void
+}
+
+const Card: FC<ICardProps> = (props) => {
+  const header = props.renderHeader ? (
+    props.renderHeader()
+  ) : (
+    <div>
+      <Styled.Title>{props.title}</Styled.Title>
+      <Styled.Subtitle>{props.subtitle}</Styled.Subtitle>
+    </div>
+  )
+
+  return (
+    <Styled.Container style={props.style}>
+      <Styled.Header className="clearfix">
+        {props.removeButton && (
+          <Styled.RemoveButton className="btn btn-link btn-xs" onClick={() => props.onRemove}>
+            <i className="fa fa-times text-danger" />
+            <span className="sr-only">Remove</span>
+          </Styled.RemoveButton>
+        )}
+        {header}
+      </Styled.Header>
+      <Styled.Body>{props.children}</Styled.Body>
+    </Styled.Container>
+  )
+}
+
+export default Card
