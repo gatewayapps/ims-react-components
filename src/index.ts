ae2e890bb33d8b6e42a4a36ced3755d60d3dc130
Daniel Gary <daniel@gatewayapps.com>
2019-10-11 08:43:16 -0500

    added new components to export

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ export { TreeViewNode } from './TreeView/TreeViewNode'
 export { ITreeViewProps } from './TreeView/ITreeViewProps'
 export { ITreeViewNodeStyleProps } from './TreeView/ITreeViewNodeStyleProps'
 export { InitialExpansionModes } from './TreeView/ITreeViewCommonProps'
+
+export { default as Card } from './Card'
+export { default as DisplayName } from './DisplayName'
+export { default as NodePath } from './NodePath'
+export { default as PersonCard } from './PersonCard'
